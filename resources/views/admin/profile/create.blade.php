6cbd43854fc0f9032410e4745debc3749499d3ea
chiho1025 <chihoasianbeauty@gmail.com>
2021-03-12 14:32:10 +0000

    php/laravel11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,6 +5,19 @@
         <meta http-equiv="X-UA-Compatible"content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>My news</title>
+        @extends('layouts.profile')
+　　　　{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+　　　　@section('title', 'Myプロフィール')
+　　　　{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+　　　　@section('content')
+    　　<div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    　　</div>
+　　　　@endsection
     </head>    
     <body>
         <h1>profile作成画面</h1>
