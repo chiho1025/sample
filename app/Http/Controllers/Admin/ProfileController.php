b5236f70be383c91b21062717565d6ba58a62766
chiho1025 <chihoasianbeauty@gmail.com>
2021-03-15 10:37:12 +0000

    課題：PHP/Laravel 14 投稿データを保存しよう

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,18 +4,26 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
     //
 
-public function add()
+    public function add()
     {
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+        
+        $profile->fil($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
 
@@ -28,4 +36,8 @@ public function add()
     {
         return redirect('admin/profile/edit');
     }
+    
+
+        
+        
 }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,52 +5,58 @@
         <meta http-equiv="X-UA-Compatible"content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>My news</title>
-        @extends('layouts.profile')
-　　　　{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-　　　　@section('title', 'Myプロフィール')
-　　　　{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
-　　　　@section('content')
-    　　<div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>Myプロフィール</h2>
-            </div>
-        </div>
-    　　</div>
-　　　　@endsection
-    </head>    
+    </head> 
+    
     <body>
-        <h1>profile作成画面</h1>
-        <form action="{{
+        @extends('layouts.profile')
+　　　　@section('title', 'Myプロフィール')
+　　　　@section('content')
+    　　   <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h1>profile作成画面</h1>
+                    <form action="{{
         action('Admin\ProfileController@create') }}"
         method="post" enctype="multipart/form-data">
-        <div class="form-group row">
-            <label class="col-md-2">氏名</label>
-                <div class="col-md-10">
-                    <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        @if (count($errors) >0)
+                            <ul>
+                                @foreach($errors->all() as $e)
+                                <li>{{ $e }}
+                                </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                        <div class="form-group row">
+                            <label class="col-md-2">氏名</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2">性別</label>
+                            <div class="col-md-10">
+                                <input type="radio" class="form-control" name="gender" value="male">男性 {{ old('gender') }}</button>
+                                <input type="radio" class="form-control" name="gender" value="male">女性 {{ old('gender') }}</button>
+                                <input type="radio" class="form-control" name="gender" value="female">その他 {{ old('gender') }}</button>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-2">趣味</label>
+                            <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="10">{{ old('hobby') }}</textarea>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                            <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                            </div>
+                        </div>
+                        {{ csrf_field() }}
+                        <input type="submit" class="btn btn-primary" value="更新">
+                    </div>
                 </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-md-2">性別</label>
-                <div class="col-md-10">
-                    <input type="radio" class="form-control" name="gender" value="male">男性 {{ old('gender') }}</button>
-                    <input type="radio" class="form-control" name="gender" value="male">女性 {{ old('gender') }}</button>
-                    <input type="radio" class="form-control" name="gender" value="female">その他 {{ old('gender') }}</button>
-                </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-md-2">趣味</label>
-                <div class="col-md-10">
-                    <textarea class="form-control" name="hobby" rows="10">{{ old('hobby') }}</textarea>
-                </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-md-2">自己紹介欄</label>
-                <div class="col-md-10">
-                    <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
-                </div>
-        </div>
-        {{ csrf_field() }}
-        <input type="submit" class="btn btn-primary" value="更新">
+    　　      </div>
+　　　　@endsection
     </body>
 </html>
