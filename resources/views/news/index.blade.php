07e44a569d7de700ea0d2e726788dafe9324158a
chiho1025 <chihoasianbeauty@gmail.com>
2021-04-11 05:25:11 +0000

    first commit

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -11,20 +11,21 @@
                             <div class="caption mx-auto">
                                 <div class="image">
                                     @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
-                                </div>
-                                <div class="title p-2">
-                                    <h1>{{ str_limit($headline->title, 70) }}</h1>
+                                        <img src="{{ $headline->image_path }}">
+                                     @endif
                                 </div>
                             </div>
+                            <div class="title p-2">
+                                <h1>{{ str_limit($headline->title, 70) }}</h1>
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
-                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
                     </div>
                 </div>
             </div>
+    </div>
         @endif
         <hr color="#c0c0c0">
         <div class="row">
@@ -44,16 +45,14 @@
                                 </div>
                             </div>
                             <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
+                                 @if ($post->image_path)
+                                    <img src="{{ $post->image_path }}">
                                 @endif
                             </div>
                         </div>
                     </div>
-                    <hr color="#c0c0c0">
-                @endforeach
             </div>
+                <hr color="#c0c0c0">
+                @endforeach
         </div>
-    </div>
-    </div>
 @endsection
